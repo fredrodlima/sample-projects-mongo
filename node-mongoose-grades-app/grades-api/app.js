e5cda62be90d1605f575fb873d9919971b6effc5
frederico.pinto <fredrodlima@gmail.com>
2020-09-06 21:08:17 +0200

    Updating urls

--- node-mongoose-grades-app/grades-api/app.js
+++ node-mongoose-grades-app/grades-api/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://grades-bootcamp-app.herokuapp.com/',
+    origin: 'https://grades-bootcamp-app.herokuapp.com',
   })
 );
 app.use(gradeRouter);
