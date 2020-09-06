aa42a7dd3fcfbf64441c022aaec17751b7580304
frederico.pinto <fredrodlima@gmail.com>
2020-09-06 20:40:17 +0200

    Updating URLS for cors and http-commong to access properly app urls

--- node-mongoose-grades-app/grades-api/app.js
+++ node-mongoose-grades-app/grades-api/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://grades-bootcamp-app.herokuapp.com/',
   })
 );
 app.use(gradeRouter);
